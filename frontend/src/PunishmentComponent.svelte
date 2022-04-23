e26dc9a7e23dab67c26718c17cec59f04a24972c
chrbachmann <christof.bachmann@aau.at>
2022-04-23 21:31:18 +0200

    Change: Display Punishment

---
+++ frontend/src/PunishmentComponent.svelte
@@ -0,0 +1,128 @@
+<script lang="ts">
+    import {createEventDispatcher} from "svelte";
+    import {Card, CardType} from "./model/card";
+    import {Punishment, PunishmentCondition} from "src/model/punishment";
+
+    export let punishment: Punishment;
+
+    function parsePunishment(punishment) {
+        switch (punishment) {
+            case PunishmentCondition.AllVotes:
+                return "Don't be that funny.";
+                break;
+            case PunishmentCondition.SameScore:
+                return "Shared sorrow is double sorrow.";
+                break;
+            case PunishmentCondition.LastToVote:
+                return "Pick faster next time.";
+                break;
+        }
+    }
+</script>
+
+<div class="col">
+    <div class="card text-center">
+        <div class="card-header punishment">
+            {#if punishment.condition === 0}Hidden{/if}Punishment
+        </div>
+        <div class="card-body">
+            {#if punishment.targets.length}
+                <h5 class="card-title text-dark">
+                    {punishment.targets.map((player) => player.name).join(', ')}
+                    {#if punishment.targets.length == 1} <br> has to
+                    {:else} <br> have to
+                    {/if}
+                </h5>
+                <h5 class="card-title text-dark">{punishment.card.text}</h5>
+                {#if punishment.condition !== 0}
+                    <h5 class="card-title text-dark">{parsePunishment(punishment.condition)}</h5>
+                {/if}
+
+            {:else}
+                <h5 class="card-title text-dark">{punishment.card.text}</h5>
+            {/if}
+
+        </div>
+
+    </div>
+</div>
+
+{#if punishment.targets.length}
+
+{:else}
+    <h1>|
+        {#each punishment.targets as target}
+            {target.name}|
+        {/each}
+        {#if punishment.targets.length == 1} has to
+        {:else} have to
+        {/if}
+    </h1>
+    <h1>
+
+    </h1>
+{/if}
+
+<style lang="scss">
+  .card {
+    position: relative;
+    top: 0;
+    transition: top ease 0.5s;
+    margin: 5px;
+
+    /* width: 14rem; */
+  }
+
+  .votedFor {
+    box-shadow: 0px 0px 25px rgba(84, 245, 51, 0.795);
+  }
+
+  .isWinner {
+    box-shadow: 0px 0px 25px rgba(240, 218, 24, 0.795);
+    color: black;
+  }
+
+  .presenterTheme {
+    font-size: 24pt;
+  }
+
+  .card-title {
+    font-family: Neucha, -apple-system, system-ui, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  }
+
+  .playable {
+    cursor: pointer;
+  }
+
+  .playable:hover {
+    color: rgba(255, 255, 255, 1);
+    box-shadow: 0 5px 5px rgba(238, 206, 24, 0.4);
+    top: -10px;
+  }
+
+  .object {
+    background-color: #6610f2;
+    color: white;
+  }
+
+  .person {
+    background-color: #fd7e14;
+    color: black;
+  }
+
+  .place {
+    background-color: #198754;
+    color: white;
+  }
+
+  .punishment {
+    background-color: #dc3545;
+    color: white;
+  }
+
+  .activity {
+    background-color: #0dcaf0;
+    color: white;
+  }
+</style>
