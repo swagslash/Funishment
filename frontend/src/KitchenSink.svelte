69052b292d68055626121989979e0a4be90892ef
Christoph Wedenig <christoph@wedenig.org>
2022-04-23 21:24:23 +0200

    Fixed Card Presenter with Played Cards

--- frontend/src/KitchenSink.svelte
+++ frontend/src/KitchenSink.svelte
@@ -2,7 +2,7 @@
     import {Question} from "./model/question";
     import QuestionComponent from "./QuestionComponent.svelte";
     import {Player} from "src/model/player";
-    import {Card, CardType} from "src/model/card";
+    import {Card, CardType, PlayedCard} from "src/model/card";
     import CardPresenter from "src/CardPresenter.svelte";
     import CardCreatorComponent from "src/CardCreatorComponent.svelte";
     import PunishmentCreatorComponent from "src/PunishmentCreatorComponent";
@@ -10,6 +10,7 @@
     import HandCards from "src/HandCards.svelte";
 
     let examplePlayer: Player = {id: '1', name: 'W8D7'};
+    let examplePlayer2: Player = {id: '2', name: 'me'};
     let exampleCard: Card = {
         type: CardType.Object,
         id: 1,
@@ -48,15 +49,26 @@
     let examplePunishment: Punishment = {
         card: exampleCardPunischment,
         targets: [examplePlayer],
-        condition : PunishmentCondition.AllVotes
+        condition: PunishmentCondition.AllVotes
     };
 
     let cards: Card[] = [exampleCard, exampleCardActivity, exampleCardPerson, exampleCardPlace]
+    let playedCards: PlayedCard[] = [
+        {
+            card: exampleCard,
+            dealer: examplePlayer,
+            votes: 0
+        },
+        {
+            card: exampleCardActivity,
+            dealer: examplePlayer2,
+            votes: 2
+        }]
     let exampleQuestion: Question = {text: "How fat is Andi's aunt?"}
 </script>
 
 <QuestionComponent question="{exampleQuestion}"></QuestionComponent>
-<CardPresenter cards="{cards}"></CardPresenter>
+<CardPresenter animationDuration={100} slideTime={200} currentPlayerId="2" playedCards="{playedCards}"></CardPresenter>
 <CardCreatorComponent punishment="{examplePunishment}"></CardCreatorComponent>
 <PunishmentCreatorComponent></PunishmentCreatorComponent>
 <HandCards canPlay={true} playerCards={cards}></HandCards>
