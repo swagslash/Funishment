e71c0d40ef67a695e2ac11539d11a0a1b471aa46
Christoph Wedenig <christoph@wedenig.org>
2022-04-24 11:53:48 +0200

    Improve texts Add continue game button

--- frontend/src/HandCards.svelte
+++ frontend/src/HandCards.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <h3>My Cards</h3>
-<p class="badge bg-warning text-black-50" hidden={!canPlay}>Select one of your cards as answer for this question
+<p class="badge bg-warning text-black-50" hidden={!canPlay}>Select one of your cards as the answer for this question
     now!</p>
 <div class="row row-cols-2 row-cols-sm-3 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-1">
     {#each cards as card}
